ac4f3100fb42de16754db7becc5db05d42fca27c
Luke Jackson <lukejacksonn@gmail.com>
2017-01-26 01:11:10 -0500

    Implements 10 second timeout for lowering quality

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 const $ = (selector) => document.querySelector(selector);
 // Set quality to auto
+let timer;
 const auto = () => {
+  clearTimeout(timer);
   $('.ytp-settings-button').click();
   $('.ytp-settings-menu .ytp-menuitem:last-child').click();
   $('.ytp-quality-menu .ytp-menuitem:last-child').click();
@@ -13,5 +15,5 @@ const low = () => {
 };
 // Listen for when document is hidden
 document.addEventListener('visibilitychange', () => {
-	document.hidden ? low() : auto();
+	!document.hidden ? auto() : timer=setTimeout(low, 10000)
 }, false);
